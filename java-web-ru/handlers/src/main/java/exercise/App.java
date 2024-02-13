44424df18ca126c26e332681004f8c7698231f4e
@hexlet/cli <support@hexlet.io>
2024-02-13 20:04:05 +0300

    submit java-web-ru/handlers

---
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -0,0 +1,34 @@
+package exercise;
+
+import io.javalin.Javalin;
+
+import java.util.List;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/phones", ctx -> {
+            List<String> phones = Data.getPhones();
+            ctx.json(phones);
+        });
+
+        app.get("/domains", ctx -> {
+            List<String> domains = Data.getDomains();
+            ctx.json(domains);
+        });
+
+        return app;
+        // END
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
